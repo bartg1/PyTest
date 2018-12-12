f5d67f1c860b0b936650a1e9166f91d6bce69b24
bartg1 <bartg1@gmail.com>
2018-12-12 08:45:44 -0800

    PrintClass Push

--- src/Pkg1/Mdl1.py
+++ src/Pkg1/Mdl1.py
@@ -1,18 +1,24 @@
-'''
-Created on Dec 11, 2018
+# objective of class is to print inbound strings
+class PrintWords():
+    def __init__(self, text1, text2, text3):
+        self.text1 = text1
+        self.text2 = text2
+        self.text3 = text3
+        print('Construction Complete')
+        
+    #def PrintTexts(self, text1, text2, text3):
+    def PrintTexts (self):
+        print(self.text1 + " | " + self.text2 + " | " + self.text3)
+        
+    def UpdateTexts(self,text1b, text2b, text3b):
+        self.text1 = text1b
+        self.text2 = text2b
+        self.text3 = text3b
 
-@author: bart.gourley
-'''
+#create a new MyClass"
+ob = PrintWords('test1','test2','test3')
 
-class MyClass(object):
-    '''
-    classdocs
-    '''
-
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
-    print(value, ___, ', end='\n', file=sys_stdout, flush=False) 
-        afadfad 
+#Actions
+ob.PrintTexts()
+ob.UpdateTexts('Updated 1b', 'Updated 2b', 'Updated 3b')
+ob.PrintTexts()
---
+++ src/Pkg1/__init__.py
@@ -0,0 +1,2 @@
+ob = Mdl1.PrintWords()
+ob.func()
