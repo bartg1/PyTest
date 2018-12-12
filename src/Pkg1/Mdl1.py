23320d841de5e1b68506188be4d258733dd197e4
bartg1 <bartg1@gmail.com>
2018-12-11 21:44:39 -0800

    First Committ

---
+++ src/Pkg1/Mdl1.py
@@ -0,0 +1,17 @@
+'''
+Created on Dec 11, 2018
+
+@author: bart.gourley
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
